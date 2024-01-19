952d47fc31d0328bfb3b4b3d9edd158b481f2659
이경용 <sleekydz86@icloud.com>
2024-01-19 21:08:39 +0900

    엘라스틱서치 Basic버전 셋팅
    엘라스틱서치 Basic버전 셋팅

---
+++ basic_elastic/src/main/java/com/study/basic_elastic/BasicElasticApplication.java
@@ -0,0 +1,13 @@
+package com.study.basic_elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicElasticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicElasticApplication.class, args);
+    }
+
+}
